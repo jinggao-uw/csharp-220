53add4d1d5262e1fb87fb912c138c6e6cf62f6b0
Jing Gao <Jing Gao>
2017-03-22 01:22:26 -0700

    Homework for assignment 4

---
+++ Homework4/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        private string zipCode;
+        public string ZipCode
+        {
+            get
+            {
+                return zipCode;
+            }
+            set
+            {
+                if(zipCode != value)
+                {
+                    zipCode = value;
+                    OnPropertyChanged("ZipCode");
+                }
+            }
+        }
+
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ Homework4/ZipCodeConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Homework4
+{
+    class ZipCodeConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string zip = value as string;
+
+            if (zip != null)
+            {
+                string USZipPattern5 = @"^([0-9]{5})$";
+                string USZipPattern9 = @"^([0-9]{5}-[0-9]{4})$";
+                string CanadaZipPattern = @"^[ABCEGHJKLMNPRSTVXYabceghjklmnprstvxy]{1}\d{1}[A-Za-z]{1} *\d{1}[A-Za-z]{1}\d{1}$";
+
+                string finalPattern = "(" + USZipPattern5 + ")|(" + USZipPattern9 + ")|(" + CanadaZipPattern + ")";
+
+                Regex regex = new Regex(finalPattern);
+                return regex.IsMatch(zip);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
