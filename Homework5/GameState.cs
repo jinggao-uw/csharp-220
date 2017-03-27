a626f1e9fa5de468fc0426ddeea67bccd251efbe
Jing Gao <Jing Gao>
2017-03-26 17:58:39 -0700

    Assignment for homework 5

---
+++ Homework5/GameState.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    public enum GamerName
+    {
+        O = 0,
+        X = 1
+    }
+
+    public class GameState : INotifyPropertyChanged
+    {
+        // Private variables
+        private const int GameSize = 3;
+        private GamerName initialStarter;
+        private GamerName?[,] matrix;
+
+        // Properties
+        public GamerName GamerInTurn { get; set; }
+        public bool GameOver { get; set; }
+        public GamerName? Winner { get; set; }
+
+        // DependencyProperties
+        private string gameMessage;
+        public string Message
+        {
+            get
+            {
+                return gameMessage;
+            }
+            set
+            {
+                if (gameMessage != value)
+                {
+                    gameMessage = value;
+                    OnPropertyChanged("Message");
+                }
+            }
+        }
+
+        // Implement INotifyPropertyChanged interface
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        // Constructor
+        public GameState(GamerName starter)
+        {
+            initialStarter = starter;
+            matrix = new GamerName?[GameSize, GameSize];
+
+            Reset();
+        }
+
+        // Reset the states
+        public void Reset()
+        {
+            GamerInTurn = initialStarter;
+            GameOver = false;
+            Message = GamerInTurn.ToString() + "'s turn";
+
+            for (int i = 0; i < GameSize; i++)
+            {
+                for (int j = 0; j < GameSize; j++)
+                {
+                    matrix[i, j] = null;
+                }
+            }
+
+            Winner = null;
+        }
+
+        // Move one step for current gamer
+        public bool MoveOneStep(int x, int y)
+        {
+            if (GameOver == true)
+            {
+                // We've already found a winner
+                Message = @"Game is over";
+                return false;
+            }
+
+            if (IsOccupiedOrInvalid(x, y))
+            {
+                // Can't place here at this step
+                Message = @"Can't place here";
+                return false;
+            }
+
+            SetOccupied(x, y);
+            Evaluate();
+
+            // flip the gamer
+            GamerInTurn = (GamerInTurn == GamerName.O) ? GamerName.X : GamerName.O;
+
+            if (GameOver == true)
+            {
+                Message = Winner.Value.ToString() + " is the winner";
+            }
+            else
+            {
+
+                Message = GamerInTurn.ToString() + "'s turn";
+            }
+
+            return true;
+        }
+
+        // Check if the passed in position is valid
+        private bool IsOccupiedOrInvalid(int x, int y)
+        {
+            if (x < 0 || y < 0 || x > GameSize - 1 || y > GameSize - 1)
+            {
+                return true;
+            }
+
+            if (matrix[x, y].HasValue)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // Mark the position in the state matrix
+        private void SetOccupied(int x, int y)
+        {
+            matrix[x, y] = GamerInTurn;
+        }
+
+        // Evaluate the current game state against the state matrix, and update the properties accordingly.
+        private void Evaluate()
+        {
+            // Check all vertical and horizontal lines
+            for (int i = 0; i < GameSize; i++)
+            {
+                GameOver = (matrix[i, 0] == GamerInTurn && matrix[i, 1] == GamerInTurn && matrix[i, 2] == GamerInTurn)
+                    || (matrix[0, i] == GamerInTurn && matrix[1, i] == GamerInTurn && matrix[2, i] == GamerInTurn);
+
+                if (GameOver)
+                {
+                    break;
+                }
+            }
+
+            // Check two cross lines
+            if (GameOver == false)
+            {
+                GameOver = (matrix[0, 0] == GamerInTurn && matrix[1, 1] == GamerInTurn && matrix[2, 2] == GamerInTurn)
+                    || (matrix[0, 2] == GamerInTurn && matrix[1, 1] == GamerInTurn && matrix[2, 0] == GamerInTurn);
+            }
+
+            // Declare the winner
+            if(GameOver)
+            {
+                Winner = GamerInTurn;
+            }
+
+            return;
+        }
+    }
+}
---
+++ Homework5/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public GameState State { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            State = new GameState(GamerName.X);
+
+            uxTurn.DataContext = State;
+        }
+
+        private void uxNewGame_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear all the units
+            ClearAllUnits();
+
+            // And reset the data model state
+            State.Reset();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+
+            string[] tags = button.Tag.ToString().Split(',');
+            if (tags.Length == 2)
+            {
+                string currentGamer = State.GamerInTurn.ToString();
+                bool result = State.MoveOneStep(System.Convert.ToInt32(tags[0]), System.Convert.ToInt32(tags[1]));
+                if (result == true)
+                {
+                    // Claim the button
+                    button.Content = currentGamer;
+                }
+            }
+            else
+            {
+                // something is wrong with the button tag, do nothing.
+            }
+
+            // This could be done via databinding, but not necessarily.
+            if (State.GameOver == true)
+            {
+                DisableAllUnits();
+            }
+        }
+
+
+        private void uxMenuExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        void ClearAllUnits()
+        {
+            foreach (var child in uxGrid.Children)
+            {
+                if (child is Button)
+                {
+                    var button = child as Button;
+                    button.Content = "";
+                    button.IsEnabled = true;
+                }
+            }
+        }
+
+        void DisableAllUnits()
+        {
+            foreach (var child in uxGrid.Children)
+            {
+                if (child is Button)
+                {
+                    var button = child as Button;
+                    button.IsEnabled = false;
+                }
+            }
+        }
+    }
+}
